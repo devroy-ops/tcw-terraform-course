c33c33a6bbba3ce4b513205de800941e51ab1fdf
devroy-ops <82015105+devroy-ops@users.noreply.github.com>
2022-01-08 14:10:27 +0530

    Update main.tf

--- lecture_3/main.tf
+++ lecture_3/main.tf
@@ -13,7 +13,6 @@ resource "aws_instance" "ec2_1" {
   ami           = "ami-0ec30f4db65328d5a"
   instance_type = "t2.micro"
   subnet_id     = data.aws_subnet.selected.id
-  key_name = dev-account
   tags = {
     Name = "HelloWorld"
   }
