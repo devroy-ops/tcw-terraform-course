33e2d64cc29cb9bab37338907201c73f9774eb07
devroy-ops <82015105+devroy-ops@users.noreply.github.com>
2022-01-08 14:09:07 +0530

    Update main.tf

--- lecture_3/main.tf
+++ lecture_3/main.tf
@@ -5,15 +5,15 @@ provider "aws"{
 data "aws_subnet" "selected" {
   filter {
     name   = "tag:Name"
-    values = [var.subnet_name]
+    values = ["subnet_2"]
   }
 }
 
 resource "aws_instance" "ec2_1" {
-  ami           = "ami-0c2b8ca1dad447f8a"
+  ami           = "ami-0ec30f4db65328d5a"
   instance_type = "t2.micro"
   subnet_id     = data.aws_subnet.selected.id
-  key_name = var.my_key
+  key_name = dev-account
   tags = {
     Name = "HelloWorld"
   }
